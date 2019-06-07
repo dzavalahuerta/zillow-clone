bdb4f8eb0988a470120f6f5981874e1f10b6967f
DanielZavalaHuerta <danielz123789@gmail.com>
2019-06-07 16:48:09 -0700

    added the dropdown on hover for the leftmost ul links on the nav, still need some finishing touches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('buyLink') buyLink;
+  @ViewChild('buyLinkDropdown') buyLinkDropdown;
   scrolledPassedFirstInput = false;
 
   constructor(){ }
@@ -17,7 +19,41 @@ export class AppComponent implements OnInit {
       }
       else{
         this.scrolledPassedFirstInput = false;
+        this.ngAfterViewInit();
       }
     });
-  } 
+  }
+  
+  ngAfterViewInit(){
+    console.log(document.querySelectorAll('a.dropdownLink'));
+    console.log(document.querySelectorAll('div.linkDropdown'));
+    const dropdownLinks = document.querySelectorAll('a.dropdownLink');
+    const linkDropdowns = document.querySelectorAll('div.linkDropdown');
+
+    for (let i = 0; i < dropdownLinks.length; i++) {
+      let link = dropdownLinks[i];
+      for (let index = 0; index < linkDropdowns.length; index++) {
+        let dropdown:any = linkDropdowns[i];
+        link.addEventListener('mouseover',()=>{
+          dropdown.style.display = 'flex';
+        });
+        link.addEventListener('mouseout',()=>{
+          dropdown.style.display = 'none';
+        });
+      }
+    }
+
+
+    let buyLink = this.buyLink.nativeElement;
+    buyLink.addEventListener('mouseover',()=>{
+      setTimeout(() => {
+        this.buyLinkDropdown.nativeElement.style.display = 'flex';
+      }, 400);
+    });
+    buyLink.addEventListener('mouseout',()=>{
+      setTimeout(() => {
+        this.buyLinkDropdown.nativeElement.style.display = 'none';
+      }, 400);
+    });
+  }
 }
